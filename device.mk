258a0d3b34b930785feb9bf369c8497954f47d42
followmsi <followmsi@gmail.com>
2018-09-26 13:20:35 +0200

    gtaxl: re-add libstagefright shim for NV21 color format

--- device.mk
+++ device.mk
@@ -98,7 +98,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libExynosOMX_shim \
-    libui_shim
+    libui_shim \
+    libstagefright_shim
 
 # Overriden service definition
 PRODUCT_COPY_FILES += \
