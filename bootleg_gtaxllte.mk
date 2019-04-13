7cf8ac7e1e1ae84cd68956a090b8dddcf77e2022
srgrusso <srgrusso240@gmail.com>
2019-04-13 11:48:26 -0500

    first commit

--- bootleg_gtaxllte.mk
+++ bootleg_gtaxllte.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_gtaxllte
