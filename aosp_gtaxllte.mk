16b5d92b7ae58285f40345a54c7549dfa3255424
srgrusso <srgrusso240@gmail.com>
2019-06-16 16:35:51 -0500

    set bootanimation resolution

--- aosp_gtaxllte.mk
+++ aosp_gtaxllte.mk
@@ -27,6 +27,9 @@ $(call inherit-product, vendor/aosp/config/common.mk)
 # Inherit some Telephony packages.
 $(call inherit-product, vendor/aosp/config/telephony.mk)
 
+# Boot animation resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := aosp_gtaxllte
 PRODUCT_DEVICE := gtaxllte
