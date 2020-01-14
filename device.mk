381e5ec757202c64ac520f64b71364bf0a2a4fd9
followmsi <followmsi@gmail.com>
2020-01-14 15:17:46 +0100

    gtaxl: Do not build SamsungDoze

--- device.mk
+++ device.mk
@@ -129,8 +129,8 @@ PRODUCT_PACKAGES += \
     android.hardware.cas@1.1-service
 
 # Doze
-PRODUCT_PACKAGES += \
-    SamsungDoze
+#PRODUCT_PACKAGES += \
+#    SamsungDoze
 
 # Touch features
 PRODUCT_PACKAGES += \
