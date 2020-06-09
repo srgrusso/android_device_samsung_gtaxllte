fa416a4ac46b2292cf5dcce01262960368430b2e
TALU <taluemail272@gmail.com>
2020-06-09 19:28:14 +0100

    lineage_gtaxllte: capitalize the manufacturers name.
    Change-Id: I8ef9323499d889ca93e35a22bc4d6896f97c06b4

--- bliss_gtaxllte.mk
+++ bliss_gtaxllte.mk
@@ -49,7 +49,7 @@ PRODUCT_NAME := bliss_gtaxllte
 PRODUCT_DEVICE := gtaxllte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-T585
-PRODUCT_MANUFACTURER := samsung
+PRODUCT_MANUFACTURER := Samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
